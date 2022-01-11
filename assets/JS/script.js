aa251260ae8d4ac3907424cc618ff6a4109ed8fe
Bradley Le <brabrele@gmail.com>
2022-01-11 04:01:56 -0800

    added humidity stats for the 5 days after the current day

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,11 +4,15 @@ var APIKey = "366a2e7bcfd3223a70d54d9bb00665b2";
 // longitude, latitude of the city
 var long;
 var lat;
+var city;
+
 // This event will occur when the user enters a city name
 // uses Geocoding API to get the latitude and longitude of the city
 $("#searchCity").click(function () {
     // stores the city name into a local variable
-    var city = $("#inputCity").val();
+    city = $("#inputCity").val();
+
+    // API used: https://openweathermap.org/api/geocoding-api
     // appends the query parameters to make a query URL
     var queryURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
 
@@ -27,13 +31,14 @@ $("#searchCity").click(function () {
         // checks for any errors
         console.log(long);
         console.log(lat);
-
-        getCurrDay();
+        // This function will get the weather statistics of the current day
+        getWeather();
     });
 });
 
-function getCurrDay() {
-
+// Function retrieves the current weather statistics of the city and displays it on the DOM
+function getWeather() {
+    // One Call API used: https://openweathermap.org/api/one-call-api
     var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" 
     + long + "&units=imperial" + "&appid=" + APIKey;
     $.ajax({
@@ -41,10 +46,29 @@ function getCurrDay() {
         method: "GET"
     }).then(function (response) {
         console.log(response);
+
+        // assigns the current day's temperatures found in the response to the corresponding html element
         $("#currTemp").text("Temperature: " + response.current.temp + " \u00B0 F");
         $("#currHumidity").text("Humidity: " + response.current.humidity + "%");
         $("#currWind").text("Wind Speed: " + response.current.wind_speed + " MPH");
         $("#currUV").text("UV Index: " + response.current.uvi);
 
+        // assigns a weather icon based on the type of weather
+        /*
+        $("#cardOneIcon").attr("src", response.daily[0].weather[0].icon + ".png");
+        $("#cardTwoIcon").attr("src", response.daily[0].weather[0].icon + ".png");
+        $("#cardThreeIcon").attr("src", response.daily[0].weather[0].icon + ".png");
+        $("#cardFourIcon").attr("src", response.daily[0].weather[0].icon + ".png");
+        $("#cardFiveIcon").attr("src", response.daily[0].weather[0].icon + ".png");
+        */
+
+        // assigns the humidity of the next 5 days to the corresponding 
+        $("#cardOneHumidity").text("Humidity: " + response.daily[0].humidity + " \u00B0 F");
+        $("#cardTwoHumidity").text("Humidity: " + response.daily[1].humidity + " \u00B0 F");
+        $("#cardThreeHumidity").text("Humidity: " + response.daily[2].humidity + " \u00B0 F");
+        $("#cardFourHumidity").text("Humidity: " + response.daily[3].humidity + " \u00B0 F");
+        $("#cardFiveHumidity").text("Humidity: " + response.daily[4].humidity + " \u00B0 F");
+
     })
 }
+
